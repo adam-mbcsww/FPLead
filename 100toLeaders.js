b892a6229c33acb8c9c3376debc2fd8a112798ad
batmaninkl <adamramlan@gmail.com>
2024-06-25 15:20:25 +0800

    100 fix

--- 100toLeaders.js
+++ 100toLeaders.js
@@ -1,18 +1,28 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  addDoc,
+  getDocs,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  query,
+  where,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBE9tWytUvjpjQ2k0CsUyIhVXC0Vpr4HxI",
-  authDomain: "leaderboard-48dfb.firebaseapp.com",
-  projectId: "leaderboard-48dfb",
-  storageBucket: "leaderboard-48dfb.appspot.com",
-  messagingSenderId: "248440155827",
-  appId: "1:248440155827:web:c7ae7daff0da23b4bc4ac6",
-  measurementId: "G-0LCDSJMXKE",
-};
+$(() => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBE9tWytUvjpjQ2k0CsUyIhVXC0Vpr4HxI",
+    authDomain: "leaderboard-48dfb.firebaseapp.com",
+    projectId: "leaderboard-48dfb",
+    storageBucket: "leaderboard-48dfb.appspot.com",
+    messagingSenderId: "248440155827",
+    appId: "1:248440155827:web:c7ae7daff0da23b4bc4ac6",
+    measurementId: "G-0LCDSJMXKE",
+  };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
 const leaderboardRef = collection(db, 'leaderboard');
 
