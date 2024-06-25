633a306dcd5f07831bc8fcf0f8196f2398cff4e2
batmaninkl <adamramlan@gmail.com>
2024-06-25 19:04:17 +0800

    show data and write new scores

--- randomScore.js
+++ randomScore.js
@@ -25,15 +25,20 @@ $(() => {
     console.log("Initializing App...");
   
     const updateScores = async (db) => {
-      console.log("Updating Scores...");
-      const userCol = collection(db, "leaderboards");
-      const userSnapshot = await getDocs(userCol);
-      userSnapshot.docs.forEach((doc) => {
-        const newScore = Math.floor(Math.random() * (9999 - 10 + 1)) + 10;
-        console.log("New Score:", newScore);
-        updateDoc(doc.ref, { score: newScore });
-      });
-    };
+        console.log("Updating Scores...");
+        const userCol = collection(db, "leaderboards");
+        const userSnapshot = await getDocs(userCol);
+        userSnapshot.docs.forEach((doc) => {
+          const newScore = Math.floor(Math.random() * (9999 - 10 + 1)) + 10;
+          console.log("New Score:", newScore);
+          try {
+            await updateDoc(doc.ref, { score: newScore });
+            console.log("Score updated successfully!");
+          } catch (error) {
+            console.error("Error updating score:", error);
+          }
+        });
+      };
   
     updateScores(db);
   });
