1a4873aea0b99491d1572379d379de89958004f3
adam-mbcsww <115698594+adam-mbcsww@users.noreply.github.com>
2024-06-25 13:13:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  addDoc,
+  getDocs,
+  collection,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+$(() => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBE9tWytUvjpjQ2k0CsUyIhVXC0Vpr4HxI",
+    authDomain: "leaderboard-48dfb.firebaseapp.com",
+    projectId: "leaderboard-48dfb",
+    storageBucket: "leaderboard-48dfb.appspot.com",
+    messagingSenderId: "248440155827",
+    appId: "1:248440155827:web:c7ae7daff0da23b4bc4ac6",
+    measurementId: "G-0LCDSJMXKE",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const getUsers = async (db) => {
+    const userCol = collection(db, "leaderboards");
+    const userSnapshot = await getDocs(userCol);
+    const userList = userSnapshot.docs.map((doc) => doc.data());
+    return userList;
+  };
+
+  const users_data = getUsers(db);
+  users_data.then((users) => {
+    const usrObj = {
+      data: [],
+    };
+    usrObj.data = users;
+
+    const usrArr = [];
+
+    usrObj.data.forEach((user) => {
+      usrArr.push([user.name, user.score, user.time]);
+    });
+    new DataTable("#table", {
+      data: usrArr,
+    });
+  });
+});
