81ddd9dc34672453badd8a4c12fa5fc4045fb18e
batmaninkl <adamramlan@gmail.com>
2024-06-25 17:01:56 +0800

    Revert

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ import {
   collection,
   serverTimestamp,
   updateDoc,
-  query,
-  where,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 $(() => {
@@ -24,27 +22,12 @@ $(() => {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
-  //// Refresh DAILY ////
   const getUsers = async (db) => {
     const userCol = collection(db, "leaderboards");
-    const startDate = new Date("2024-06-25T00:00:00.000Z"); // DAILY adjust this to the desired date
-    const q = query(userCol, where("timestamp", ">=", startDate));
-    const userSnapshot = await getDocs(q);
+    const userSnapshot = await getDocs(userCol);
     const userList = userSnapshot.docs.map((doc) => doc.data());
     return userList;
   };
-
-  //// Refresh WEEKLY ////
-  // const getUsers = async (db) => {
-  //   const userCol = collection(db, "leaderboards");
-  //   const startDate = new Date("2024-06-16T00:00:00.000Z"); // get current date
-  //   startDate.setDate(startDate.getDate() - startDate.getDay()); // set to first day of the week
-  //   const endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000); // set to last day of the week
-  //   const q = query(userCol, where("timestamp", ">=", startDate), where("timestamp", "<", endDate));
-  //   const userSnapshot = await getDocs(q);
-  //   const userList = userSnapshot.docs.map((doc) => doc.data());
-  //   return userList;
-  // };
 
   const users_data = getUsers(db);
   users_data.then((users) => {
@@ -55,21 +38,14 @@ $(() => {
 
     const usrArr = [];
 
-    usrObj.data.forEach((user) => { 
-      usrArr.push([user.name, user.score, user.time, user.created]);
+    usrObj.data.forEach((user) => {
+      usrArr.push([user.name, user.score, user.time, user.timestamp]);
     });
 
-    $(document).ready(function () {
-      $("#table").DataTable({
-        data: usrArr,
-        columns: [
-          { title: "Name" },
-          { title: "Score" },
-          { title: "Time" },
-          { title: "Created" },
-        ],
-        order: [[1, "desc"]], // sort by score in descending order
-      });
+ 
+
+    new DataTable("#table", {
+      data: usrArr,
     });
   });
 });
