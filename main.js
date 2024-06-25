c53e8e751e1c65fb63bb729f06162e15b22b8b72
batmaninkl <adamramlan@gmail.com>
2024-06-25 14:03:54 +0800

    CDN fix

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-import { initializeApp } from './node_modules/firebase/app'; 
-import { getFirestore, addDoc, getDocs, collection, serverTimestamp } from './node_modules/firebase/firestore';
+import firebase from 'https://cdn.firebase.com/libs/firebasejs/9.6.1/firebase-app.js';
+import 'https://cdn.firebase.com/libs/firebasejs/9.6.1/firebase-firestore.js';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBE9tWytUvjpjQ2k0CsUyIhVXC0Vpr4HxI",
-  authDomain: "leaderboard-48dfb.firebaseapp.com",
-  projectId: "leaderboard-48dfb",
-  storageBucket: "leaderboard-48dfb.appspot.com",
-  messagingSenderId: "248440155827",
-  appId: "1:248440155827:web:c7ae7daff0da23b4bc4ac6",
-  measurementId: "G-0LCDSJMXKE",
-};
+$(() => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBE9tWytUvjpjQ2k0CsUyIhVXC0Vpr4HxI",
+    authDomain: "leaderboard-48dfb.firebaseapp.com",
+    projectId: "leaderboard-48dfb",
+    storageBucket: "leaderboard-48dfb.appspot.com",
+    messagingSenderId: "248440155827",
+    appId: "1:248440155827:web:c7ae7daff0da23b4bc4ac6",
+    measurementId: "G-0LCDSJMXKE",
+  };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore(app);
 
   const getUsers = async (db) => {
-    const userCol = collection(db, "leaderboards");
-    const userSnapshot = await getDocs(userCol);
+    const userCol = db.collection("leaderboards");
+    const userSnapshot = await userCol.get();
     const userList = userSnapshot.docs.map((doc) => doc.data());
     return userList;
   };
@@ -32,9 +32,10 @@ const db = getFirestore(app);
     const usrArr = [];
 
     usrObj.data.forEach((user) => {
-      usrArr.push([user.name, user.score, user.time, user.timestamp.toDate().toLocaleString()]);
+      usrArr.push([user.name, user.score, user.time, firebase.firestore.FieldValue.serverTimestamp()]);
     });
     new DataTable("#table", {
       data: usrArr,
     });
-  }); 
+  });
+});
